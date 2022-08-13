549d75fec0caf8effbfcfef80df8c2e90478d39d
Austin Hood <Austinmhood@live.com>
2022-08-13 04:22:24 -0400

    finally used git add.

---
+++ components/Navbar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+    return(
+        <div className="w-screen h-[80px] z-10 bg bg-zinc-200 fixed drop-shadow-lg">
+            <div className="flex items-center justify-between w-full h-full px-2 ">
+                <div className="flex items-center">
+                    <h1 className="mr-4 text-3xl font-bold sm:text-4xl">BRAND.</h1>
+                    <ul className="flex items-center margin">
+                        <li>About</li>
+                        <li>Projects</li>
+                        <li>Work</li>
+                        <li>Contact</li>
+                        <li>Resume</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
