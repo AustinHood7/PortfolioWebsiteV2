3aee78c3d51c5ca6869e01b87e5fc8b02083d1ca
Austin Hood <Austinmhood@live.com>
2022-08-24 02:59:07 -0400

    Added more text and styling to the landing page

--- components/LandingPage.jsx
+++ components/LandingPage.jsx
@@ -5,14 +5,18 @@ import Image from 'next/image';
 
 const LandingPage = () => {
     return (
-        <div className="flex flex-col justify-between w-full h-screen bg-gray-900">
-            <div className="grid md:grid-cols-2 max-w-[1240] m-auto">
-                <div className="flex flex-col justify-center w-full px-2 py-4 md:items-start">
-                    <p className="my-2 text-2xl text-vice">Hi, my name is </p>
-                    <h1 className="p-2 font-body text-primary text-8xl md:flex text-shadow-blue">Austin Hood</h1>
-                    <p>This is our tech brand.</p>
-                    <button>Get Started</button>
+        <div className="flex justify-between w-full h-screen bg-gray-900">
+            <div className="grid max-w-full m-auto sm:grid-cols-2">
+                <div className="flex flex-col justify-center w-full px-2 py-4">
+                    <p className="my-2 text-3xl font-bold font-body text-vice">Hi, my name is </p>
+                    <h1 className="p-5 font-body text-primary text-8xl md:flex text-shadow-blue">Austin Hood</h1>
+                    <p className="my-2 text-4xl font-extrabold font-alt text-sky-200">I make software for the web. </p>
+                    <p className="font-extrabold font font-alt text-sky-200">
+                        I’m a senior Software Engineering student who specializes in building and designing web applications. 
+                        Right now, I’m focused on building a web based social networking platform for stock trading at soleindustries
+                    </p>
                 </div>
+                
             </div>
         </div>
     )
