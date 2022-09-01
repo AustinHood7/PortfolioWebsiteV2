d83d12e9d1522b9b71b9b0a8fc45016383140f86
Austin Hood <Austinmhood@live.com>
2022-08-31 20:38:42 -0400

    Added logos and href to the fixed sidebar

--- components/LandingPage.jsx
+++ components/LandingPage.jsx
@@ -6,8 +6,22 @@ import Image from 'next/image';
 const LandingPage = () => {
     return (
         <div className="grid w-full h-screen bg-gray-900 grid-cols-custom">
-            <div className="fixed top-[90%] left-10 text-primary">
-                <img src="github.png" alt="This is github"></img>
+            <div className="fixed top-[75%] left-10 text-primary">
+                <a href="https://github.com/AustinHood7">
+                    <div className="pb-3">
+                        <img src="resume.png" alt="This is resume"></img>
+                    </div>
+                </a>
+                <a href="https://github.com/AustinHood7">
+                    <div className="pb-3">
+                        <img src="github.png" alt="This is github"></img>
+                    </div>
+                </a>
+                <a href="https://www.linkedin.com/in/austin-hood7/">
+                    <div className="pb-2">
+                        <img src="linkedinBlue.png" alt="This is github"></img>
+                    </div>
+                </a>
             </div>
             <div className="max-w-full col-span-2 col-start-2 py-4 pl-[10rem] m-auto sm:grid-cols-2">
                 <p className="py-5 my-2 text-2xl font-body text-sky-200">Hi, my name is </p>
