9a2089686d50f029ab62894bdc1e2ab052d05992
Austin Hood <Austinmhood@live.com>
2022-09-02 03:12:46 -0400

    Made a sticky menu for the right side of the web page for fast emails

--- components/VerticalEmail.jsx
+++ components/VerticalEmail.jsx
@@ -5,10 +5,13 @@ import Image from 'next/image';
 
 const VerticalEmail = () => {
     return (
-        <div className="fixed top-[65%] right-[3%] text-zinc-200">
-            <div className="rotate-90">
-                AustinMHood@Live.com
-            </div>
+        <div className="fixed top-[83%] right-[3%] text-primary">
+            <a href="https://github.com/AustinHood7">
+                <div className="pb-3">
+                    <img src="icons8-circled-envelope-50 (1).png" alt="This is resume"></img>
+                </div>
+            </a>
+            <div className="h-[150px] border-l-[.15rem] border-zinc-200 ml-[25px]"></div>
         </div>
     )
 }
