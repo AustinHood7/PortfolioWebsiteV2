dc0bb5572b7e817a3d56ceda064ad82a70708286
Austin Hood <Austinmhood@live.com>
2022-09-12 03:57:03 -0400

    Fixed some of the spacing in the nav and made assets disappear when in mobile mode.

--- components/LandingPage.jsx
+++ components/LandingPage.jsx
@@ -2,7 +2,7 @@
 const LandingPage = () => {
     return (
         <div className="grid w-full h-screen bg-gray-900 grid-cols-custom">
-            <div className="max-w-full col-span-3 col-start-2 pl-[4rem] m-auto mt-[12rem] sm:grid-cols-2">
+            <div className="max-w-full col-span-3 col-start-2 pl-[2rem] m-auto mt-[12rem] sm:grid-cols-2">
                 <p className="py-5 my-2 text-2xl font-body text-sky-200">Hi, my name is </p>
                 <h1 className="py-5 font-body text-primary text-8xl md:flex text-shadow-blue">Austin Hood</h1>
                 <p className="py-5 my-2 text-6xl font-extrabold font-body text-vice">I make software for the web. </p>
@@ -12,7 +12,7 @@ const LandingPage = () => {
                     <span className="text-primary text-shadow-blue"> SoleIndustries.com</span>
                 </p>
             </div>
-            <span className="col-span-2 mt-[17rem] z-0 translate-x-[-8%]"><img src="whale.png" className="z-0"></img></span>
+            <span className="col-span-2 mt-[17rem] z-0 translate-x-[-8%] hidden md:block"><img src="whale.png" className="z-0"></img></span>
         </div>
     )
 }
