bf033651d62da3cb7d04b46022c753cad3b44c55
Austin Hood <Austinmhood@live.com>
2022-09-18 14:49:33 -0400

    Began to style the about me page and reusable page components for the rest of the website

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -5,8 +5,11 @@ import Image from 'next/image';
 
 const AboutMe = () => {
     return (
-        <div className="grid w-full h-screen bg-gray-900 grid-cols-5">
-            
+        <div className="grid w-full h-screen grid-cols-5 bg-gray-900 text-sky-200">
+            <div className="col-start-1 col-span-5 text-4xl font-bold text-center transition-all duration-200 text-primary mt-[10rem]">00. About me<hr className="col-span-2 col-start-4"></hr>
+</div>
+            <span className="col-span-3 col-start-2">I am a software engineering student at FGCU looking for any opportunity to help me grow to be the enginner I have always dreamed of being</span>
+
         </div>
     )
 }
